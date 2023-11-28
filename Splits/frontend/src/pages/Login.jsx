7d1364e65eb0bcf85bc4fee89436feb35993b473
Haroon Frotan <haroon_fro@hotmail.com>
2023-11-28 13:54:33 -0500

    register forum working

--- Splits/frontend/src/pages/Login.jsx
+++ Splits/frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div className="w-full flex justify-center items-center h-[70vh]">
+        <div className="w-full flex justify-center items-center h-[80vh]">
             <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
               <h1 className="text-xl font-bold text-left">Log In</h1>
               <input className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email" />
