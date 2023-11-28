c3d0f2afa1f42e8dd8849d918200b04d823a67c0
Haroon Frotan <haroon_fro@hotmail.com>
2023-11-28 11:21:59 -0500

    navbar done

--- Splits/frontend/src/components/MainPosts.jsx
+++ Splits/frontend/src/components/MainPosts.jsx
@@ -1,7 +1,26 @@
 
 const MainPosts = () => {
     return (
-        <div>MainPosts</div>
+        <div className="w-full flex mt-8 space-x-4">
+        <div className="w-[35%] h-[200px] flex justify-center items-center">
+        <img src="" alt="" className=""/>
+        </div>
+
+        <div className="flex flex-col w-[65%]">
+          <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
+            3 Uses of Push-Pull-Leg Split
+          </h1>
+          <div className="flex mb-2 text-sm font-semibold text-grey-500 items-center justify-between md:mb-4">
+           <p>@HaroonFrotan</p>
+           <div className="flex space-x-2">
+            <p>11/28/23</p>
+            <p>11:16</p>
+           </div>
+          </div>
+          <p className="text-sm md:text-lg">Push Pull Legs is a split that can be used 5 times a week with 2 rest days in between</p>
+        </div>
+
+        </div>
     )
 }
 
