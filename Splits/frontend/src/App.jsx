b5d75c9f07cb32f699406e9cccee1c4be0b7cd1b
Haroon Frotan <haroon_fro@hotmail.com>
2023-11-30 11:02:45 -0500

    createpost section is good

--- Splits/frontend/src/App.jsx
+++ Splits/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import SplitDetails from './pages/SplitDetails'
+import CreatePost from './pages/CreatePost'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
+      <Route exact path="/write" element={<CreatePost/>}/>
       <Route exact path="/posts/post/:id" element={<SplitDetails/>}/>
       </Routes>
       
