d8dab1238b413c81b7ed4934990ac3b844f7206b
Haroon Frotan <haroon_fro@hotmail.com>
2023-11-30 14:23:10 -0500

    comment page is good

--- Splits/frontend/src/pages/SplitDetails.jsx
+++ Splits/frontend/src/pages/SplitDetails.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import { BiEdit } from 'react-icons/bi'
 import { MdDelete } from 'react-icons/md'
+import Comment from "../components/Comment"
 
 
 const SplitDetails = () => {
@@ -36,38 +37,8 @@ const SplitDetails = () => {
        </div>
        <div className="flex flex-col mt-4">
         <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
-        {/* comment */}
-        <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
-        <div className="flex items-center justify-between">
-            <h3 className="font-bold text-gray-600">@HaroonFrotan</h3>
-            <div className="flex justify-center items-center space-x-4">
-                <p className="text-gray-500 text-sm">11/29/23</p>
-                <p className="text-gray-500 text-sm">11:53</p>
-                <div className="flex items-center justify-center space-x-2">
-                    <p><BiEdit/></p>
-                    <p><MdDelete/></p>
-            </div>
-            </div>
-        </div>
-        <p className="px-4 mt-2">Great Info!!</p>
-
-        </div>
-         {/* comment */}
-         <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
-        <div className="flex items-center justify-between">
-            <h3 className="font-bold text-gray-600">@HaroonFrotan</h3>
-            <div className="flex justify-center items-center space-x-4">
-                <p className="text-gray-500 text-sm">11/29/23</p>
-                <p className="text-gray-500 text-sm">11:53</p>
-                <div className="flex items-center justify-center space-x-2">
-                    <p><BiEdit/></p>
-                    <p><MdDelete/></p>
-            </div>
-            </div>
-        </div>
-        <p className="px-4 mt-2">Great Info!!</p>
-
-        </div>
+       <Comment/>
+       <Comment/>
        </div>
        {/* write a new comment */}
        <div className="w-full flex flex-col mt-4 md:flex-row">
