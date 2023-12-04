33681c2379296a60203e3bec537f4f0e0fbec918
Haroon Frotan <haroon_fro@hotmail.com>
2023-12-04 16:06:52 -0500

    connected to atlas DB' clear

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 
 
+//database
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://haroon_fro:Supundam11@cluster0.eqy8gae.mongodb.net/Splits?retryWrites=true&w=majority")
+        console.log('Database is Connected!')
+    }
+    catch(err){
+        console.log('Error!')
+    }
+} 
 
 app.listen(3001, () => {
+    connectDB()
     console.log('Server running on port 3001!')
 })
