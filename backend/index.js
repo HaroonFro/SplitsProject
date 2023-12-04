830be16c96fb1a25321b96afcb18eb4e7787b92c
Haroon Frotan <haroon_fro@hotmail.com>
2023-12-04 16:15:25 -0500

    created env folder to hide link password

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
+const dotenv = require('dotenv')
+
 
 
 //database
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://haroon_fro:Supundam11@cluster0.eqy8gae.mongodb.net/Splits?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('Database is Connected!')
     }
     catch(err){
         console.log('Error!')
     }
 } 
+
+dotenv.config()
 
 app.listen(3001, () => {
     connectDB()
